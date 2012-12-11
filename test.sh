af7f80d03d84a98c581f88e81ce79271683233ad
devandy <andy.biagini@gmail.com>
2012-12-11 16:05:04 +0100

    asd

--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
 clear
-# mocha -t 10000 ./test/*.coffee --require should --compilers coffee:coffee-script -R spec
-coffee --compile --output test/lib/ test/src/
-./node_modules/mocha-phantomjs/bin/mocha-phantomjs /home/andy/dev/js/pol-alpha/test/test.html
+#coffee -c ./test/login_test.coffee
+mocha -t 10000 ./test/*.coffee --require should --compilers coffee:coffee-script -R spec
