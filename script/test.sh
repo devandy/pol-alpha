2eee5f418cd7bcf93c8b80c5622364518567bb2a
devandy <andy.biagini@gmail.com>
2012-12-13 10:50:42 +0100

    sistemati gli script

--- script/test.sh
+++ script/test.sh
@@ -1,4 +1,5 @@
 clear
+echo "$0"
 coffee --compile --output test/lib/ test/src/ 
 #mocha -t 10000 ./test/*.coffee --require should --compilers coffee:coffee-script -R spec
 phantomjs node_modules/mocha-phantomjs/lib/mocha-phantomjs.coffee test/test_runner.html
